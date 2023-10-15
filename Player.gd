34e89eab1f259441226e6bbd554acf5583275240
Neto <100791626+mneet@users.noreply.github.com>
2023-10-15 14:07:57 -0300

    Level and player movement

--- Player.gd
+++ Player.gd
@@ -1,19 +1,22 @@
-extends Node3D
+extends RigidBody3D
 
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-	
-
+## How much vertical force to apply when moving.
+@export_range(750.0, 3000.0) var thrust: float = 1000.0
+@export var torque_thrust: float = 100.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
-		position.y += delta
-	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
-	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * delta * thrust)
+	if Input.is_action_pressed("rotate_left"):
+		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta)) 
+	if Input.is_action_pressed("rotate_right"):
+		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
 		
+
+
+func _on_body_entered(body: Node) -> void:
+	if "Goal" in body.get_groups():
+		print(body.name)
+	elif "Hazard" in body.get_groups():
+		print("morri")
